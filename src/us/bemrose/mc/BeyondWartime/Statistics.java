2518aa2e268ee50993495eb07ce7146271f6ec7f
Lihad <kylecubed@comcast.net>
2012-08-31 01:09:46 -0700

    fixed nps

--- src/us/bemrose/mc/BeyondWartime/Statistics.java
+++ src/us/bemrose/mc/BeyondWartime/Statistics.java
@@ -123,70 +123,118 @@ public class Statistics implements Listener {
 			resetDamageReceived();
 		}
 	}
-	
+
 	public static int getNodesCaptured(Player player){return nodescaptured.get(player.getName());}
-	public static void incrementNodesCaptured(Player player){nodescaptured.put(player.getName(), nodescaptured.get(player.getName())+1);}
-	public static void incrementNodesCapturedByX(Player player, int x){nodescaptured.put(player.getName(), nodescaptured.get(player.getName())+x);}
+	public static void incrementNodesCaptured(Player player){
+		if(nodescaptured.get(player.getName()) == null)setNodesCaptured(player,1);
+		else nodescaptured.put(player.getName(), nodescaptured.get(player.getName())+1);}
+	public static void incrementNodesCapturedByX(Player player, int  x){
+		if(nodescaptured.get(player.getName()) == null)setNodesCaptured(player,x);
+		else nodescaptured.put(player.getName(), nodescaptured.get(player.getName())+x);}
 	public static void setNodesCaptured(Player player, int x){nodescaptured.put(player.getName(), x);}
-	
+
 	public static int getKills(Player player){return kills.get(player.getName());}
-	public static void incrementKills(Player player){kills.put(player.getName(), kills.get(player.getName())+1);}
-	public static void incrementKillsByX(Player player, int x){kills.put(player.getName(), kills.get(player.getName())+x);}
+	public static void incrementKills(Player player){
+		if(kills.get(player.getName()) == null)setKills(player,1);
+		else kills.put(player.getName(), kills.get(player.getName())+1);}
+	public static void incrementKillsByX(Player player, int x){
+		if(kills.get(player.getName()) == null)setKills(player,x);
+		else kills.put(player.getName(), kills.get(player.getName())+x);}
 	public static void setKills(Player player, int x){kills.put(player.getName(), x);}
 	public static void resetKills(){kills.clear();}
 
 	public static int getDeaths(Player player){return deaths.get(player.getName());}
-	public static void incrementDeaths(Player player){deaths.put(player.getName(), ((int)deaths.get(player.getName()))+1);}
-	public static void incrementDeathsByX(Player player, int x){deaths.put(player.getName(), deaths.get(player.getName())+x);}
+	public static void incrementDeaths(Player player){
+		if(deaths.get(player.getName()) == null)setDeaths(player,1);
+		else deaths.put(player.getName(), (deaths.get(player.getName()))+1);}
+	public static void incrementDeathsByX(Player player, int x){
+		if(kills.get(player.getName()) == null)setDeaths(player,x);
+		else deaths.put(player.getName(), deaths.get(player.getName())+x);}
 	public static void setDeaths(Player player, int x){deaths.put(player.getName(), x);}
 	public static void resetDeaths(){deaths.clear();}
-	
+
 	public static int getTotalKills(Player player){return totalkills.get(player.getName());}
-	public static void incrementTotalKills(Player player){totalkills.put(player.getName(), totalkills.get(player.getName())+1);}
-	public static void incrementTotalKillsByX(Player player, int x){totalkills.put(player.getName(), totalkills.get(player.getName())+x);}
+	public static void incrementTotalKills(Player player){
+		if(totalkills.get(player.getName()) == null)setTotalKills(player,1);
+		else totalkills.put(player.getName(), totalkills.get(player.getName())+1);}
+	public static void incrementTotalKillsByX(Player player, int x){
+		if(kills.get(player.getName()) == null)setTotalKills(player,x);
+		else totalkills.put(player.getName(), totalkills.get(player.getName())+x);}
 	public static void setTotalKills(Player player, int x){totalkills.put(player.getName(), x);}
 
 	public static int getTotalDeaths(Player player){return totaldeaths.get(player.getName());}
-	public static void incrementTotalDeaths(Player player){totaldeaths.put(player.getName(), totaldeaths.get(player.getName())+1);}
-	public static void incrementTotalDeathsByX(Player player, int x){totaldeaths.put(player.getName(), totaldeaths.get(player.getName())+x);}
+	public static void incrementTotalDeaths(Player player){
+		if(totaldeaths.get(player.getName()) == null)setTotalDeaths(player,1);
+		else totaldeaths.put(player.getName(), totaldeaths.get(player.getName())+1);}
+	public static void incrementTotalDeathsByX(Player player, int x){
+		if(totaldeaths.get(player.getName()) == null)setTotalDeaths(player,x);
+		else totaldeaths.put(player.getName(), totaldeaths.get(player.getName())+x);}
 	public static void setTotalDeaths(Player player, int x){totaldeaths.put(player.getName(), x);}
-	
+
 	public static int getWins(Player player){return wins.get(player.getName());}
-	public static void incrementWins(Player player){wins.put(player.getName(), wins.get(player.getName())+1);}
-	public static void incrementWinsByX(Player player, int x){wins.put(player.getName(), wins.get(player.getName())+x);}
+	public static void incrementWins(Player player){
+		if(wins.get(player.getName()) == null)setWins(player,1);
+		else wins.put(player.getName(), wins.get(player.getName())+1);}
+	public static void incrementWinsByX(Player player, int x){
+		if(wins.get(player.getName()) == null)setWins(player,x);
+		else wins.put(player.getName(), wins.get(player.getName())+x);}
 	public static void setWins(Player player, int x){wins.put(player.getName(), x);}
 
 	public static int getLosses(Player player){return losses.get(player.getName());}
-	public static void incrementLosses(Player player){losses.put(player.getName(), losses.get(player.getName())+1);}
-	public static void incrementLossesByX(Player player, int x){losses.put(player.getName(), losses.get(player.getName())+x);}
+	public static void incrementLosses(Player player){
+		if(losses.get(player.getName()) == null)setLosses(player,1);
+		else losses.put(player.getName(), losses.get(player.getName())+1);}
+	public static void incrementLossesByX(Player player, int x){
+		if(losses.get(player.getName()) == null)setLosses(player,x);
+		else losses.put(player.getName(), losses.get(player.getName())+x);}
 	public static void setLosses(Player player, int x){losses.put(player.getName(), x);}
-	
+
 	public static int getDamageDealt(Player player){return damagedealt.get(player.getName());}
-	public static void incrementDamageDealt(Player player){damagedealt.put(player.getName(), damagedealt.get(player.getName())+1);}
-	public static void incrementDamageDealtByX(Player player, int x){damagedealt.put(player.getName(), damagedealt.get(player.getName())+x);}
+	public static void incrementDamageDealt(Player player){
+		if(damagedealt.get(player.getName()) == null)setDamageDealt(player,1);
+		else damagedealt.put(player.getName(), damagedealt.get(player.getName())+1);}
+	public static void incrementDamageDealtByX(Player player, int x){
+		if(damagedealt.get(player.getName()) == null)setDamageDealt(player,x);
+		else damagedealt.put(player.getName(), damagedealt.get(player.getName())+x);}
 	public static void setDamageDealt(Player player, int x){damagedealt.put(player.getName(), x);}
 	public static void resetDamageDealt(){damagedealt.clear();}
 
 	public static int getDamageReceived(Player player){return damagereceived.get(player.getName());}
-	public static void incrementDamageReceived(Player player){damagereceived.put(player.getName(), damagereceived.get(player.getName())+1);}
-	public static void incrementDamageReceivedByX(Player player, int x){damagereceived.put(player.getName(), damagereceived.get(player.getName())+x);}
+	public static void incrementDamageReceived(Player player){
+		if(damagereceived.get(player.getName()) == null)setDamageReceived(player,1);
+		else damagereceived.put(player.getName(), damagereceived.get(player.getName())+1);}
+	public static void incrementDamageReceivedByX(Player player, int x){
+		if(damagereceived.get(player.getName()) == null)setDamageReceived(player,x);
+		else damagereceived.put(player.getName(), damagereceived.get(player.getName())+x);}
 	public static void setDamageReceived(Player player, int x){damagereceived.put(player.getName(), x);}
 	public static void resetDamageReceived(){damagereceived.clear();}
-	
+
 	public static int getTotalDamageDealt(Player player){return totaldamagedealt.get(player.getName());}
-	public static void incrementTotalDamageDealt(Player player){totaldamagedealt.put(player.getName(), totaldamagedealt.get(player.getName())+1);}
-	public static void incrementTotalDamageDealtByX(Player player, int x){totaldamagedealt.put(player.getName(), totaldamagedealt.get(player.getName())+x);}
+	public static void incrementTotalDamageDealt(Player player){
+		if(totaldamagedealt.get(player.getName()) == null)setTotalDamageDealt(player,1);
+		else totaldamagedealt.put(player.getName(), totaldamagedealt.get(player.getName())+1);}
+	public static void incrementTotalDamageDealtByX(Player player, int x){
+		if(totaldamagedealt.get(player.getName()) == null)setTotalDamageDealt(player,x);
+		else totaldamagedealt.put(player.getName(), totaldamagedealt.get(player.getName())+x);}
 	public static void setTotalDamageDealt(Player player, int x){totaldamagedealt.put(player.getName(), x);}
 
 	public static int getTotalDamageReceived(Player player){return totaldamagereceived.get(player.getName());}
-	public static void incrementTotalDamageReceived(Player player){totaldamagereceived.put(player.getName(), totaldamagereceived.get(player.getName())+1);}
-	public static void incrementTotalDamageReceivedByX(Player player, int x){totaldamagereceived.put(player.getName(), totaldamagereceived.get(player.getName())+x);}
+	public static void incrementTotalDamageReceived(Player player){
+		if(totaldamagereceived.get(player.getName()) == null)setTotalDamageReceived(player,1);
+		else totaldamagereceived.put(player.getName(), totaldamagereceived.get(player.getName())+1);}
+	public static void incrementTotalDamageReceivedByX(Player player, int x){
+		if(totaldamagereceived.get(player.getName()) == null)setTotalDamageReceived(player,x);
+		else totaldamagereceived.put(player.getName(), totaldamagereceived.get(player.getName())+x);}
 	public static void setTotalDamageReceived(Player player, int x){totaldamagereceived.put(player.getName(), x);}
 
 	public static int getTimePlayed(Player player){return timeplayed.get(player.getName());}
-	public static void incrementTimePlayed(Player player){timeplayed.put(player.getName(), timeplayed.get(player.getName())+1);}
-	public static void incrementTimePlayedByX(Player player, int x){timeplayed.put(player.getName(), timeplayed.get(player.getName())+x);}
+	public static void incrementTimePlayed(Player player){
+		if(timeplayed.get(player.getName()) == null)setTimePlayed(player,1);
+		else timeplayed.put(player.getName(), timeplayed.get(player.getName())+1);}
+	public static void incrementTimePlayedByX(Player player, int x){
+		if(timeplayed.get(player.getName()) == null)setTimePlayed(player,x);
+		else timeplayed.put(player.getName(), timeplayed.get(player.getName())+x);}
 	public static void setTimePlayed(Player player, int x){timeplayed.put(player.getName(), x);}
 
-	
+
 }
